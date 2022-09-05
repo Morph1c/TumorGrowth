3d802e3e97cf359f24f2d57cc892064a6ff84a92
Morph1c <andreatocca2503@gmail.com>
2022-09-05 17:36:21 +0200

    Changing library for tensor manipulation
    Code does not working yet!!

--- tumor.cpp
+++ tumor.cpp
@@ -10,11 +10,12 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include </Users/andrea/desktop/ftensor/src/FTensor.hpp>
 
 // constant definition
 #define L_grid 50
 #define alpha 3/L_grid
-#define MAX_ITER_TIME 1000
+#define MAX_ITER_TIME 10
 #define V 1
 #define delta_x 1
 #define delta_t 0.02
@@ -42,14 +43,14 @@ class CellularAutomata{
 };
 
 // FUNCTION DEFINITON
-double k_N(int k, int i, int j, vector<vector<vector<double> > > cancer_cells, vector<vector<vector<double> > > normal_cells){
+double k_N(int k, int i, int j, FTensor::Tensor3<double, MAX_ITER_TIME, L_grid, L_grid> cancer_cells, FTensor::Tensor3<double, MAX_ITER_TIME, L_grid, L_grid> normal_cells){
 
-	return (alpha * alpha) * normal_cells[k][i][j] + lambda_N * (alpha * alpha) * cancer_cells[k][i][j];
+	return (alpha * alpha) * normal_cells(k, i, j) + lambda_N * (alpha * alpha) * cancer_cells(k, i, j);
 }
 
-double k_M(int k, int i, int j, vector<vector<vector<double> > > cancer_cells, vector<vector<vector<double> > > normal_cells){
+double k_M(int k, int i, int j, FTensor::Tensor3<double, MAX_ITER_TIME, L_grid, L_grid> cancer_cells, FTensor::Tensor3<double, MAX_ITER_TIME, L_grid, L_grid> normal_cells){
 
-	return (alpha * alpha) * normal_cells[k][i][j] + lambda_M * (alpha * alpha) * cancer_cells[k][i][j];
+	return (alpha * alpha) * normal_cells(k, i, j) + lambda_M * (alpha * alpha) * cancer_cells(k, i, j);
 }
 
 
@@ -62,22 +63,21 @@ int main(void){
 	double gamma = (V * delta_t) / (delta_x * delta_x);
 
 	// definisco tensori utili per la simulazione
-	vector<vector<vector<double> > > cancer_cells(MAX_ITER_TIME, vector<vector<double> > (cells_number, vector<double>(cells_number,1)));
-	vector<vector<vector<double> > > normal_cells(MAX_ITER_TIME, vector<vector<double> > (cells_number, vector<double>(cells_number,1)));
-	vector<vector<vector<double> > > death_cells(MAX_ITER_TIME, vector<vector<double> > (cells_number, vector<double>(cells_number,1)));
-	vector<vector<vector<double> > > N(MAX_ITER_TIME, vector<vector<double> > (cells_number, vector<double>(cells_number,1)));
-	vector<vector<vector<double> > > M(MAX_ITER_TIME, vector<vector<double> > (cells_number, vector<double>(cells_number,1)));
-
+	FTensor::Tensor3 <double, MAX_ITER_TIME, L_grid, L_grid> cancer_cells;
+	FTensor::Tensor3 <double, MAX_ITER_TIME, L_grid, L_grid> normal_cells;
+	FTensor::Tensor3 <double, MAX_ITER_TIME, L_grid, L_grid> death_cells;
+	FTensor::Tensor3 <double, MAX_ITER_TIME, L_grid, L_grid> N;
+	FTensor::Tensor3 <double, MAX_ITER_TIME, L_grid, L_grid> M;
 
 
 
 	for(int k = 0; k < MAX_ITER_TIME - 1; k++){
 		cout << "Time: " << k << endl;
-		for(int i = 1; i < cells_number - 1; i++){
-			for(int j = 1; j < cells_number - 1; j++){
+		for(int i = 1; i < L_grid - 1; i++){
+			for(int j = 1; j < L_grid - 1; j++){
 				cout << "updating: " << i << " " << j << endl;
-				N[k + 1][i][j] = gamma * (N[k][i + 1][j] + N[k][i-1][j] + N[k][i][j + 1] + N[k][i][j-1] - 4*N[k][i][j]) + N[k][i][j] - delta_t*N[k][i][j]*k_N(k, i, j, cancer_cells, normal_cells);
-                M[k + 1][i][j] = gamma * (M[k][i + 1][j] + M[k][i-1][j] + M[k][i][j + 1] + M[k][i][j-1] - 4*M[k][i][j]) + M[k][i][j] - delta_t*M[k][i][j]*k_M(k, i, j, cancer_cells, normal_cells);
+				N(k + 1, i, j) = gamma * (N(k, i + 1, j) + N(k, i-1, j) + N(k, i, j + 1) + N(k, i, j-1) - 4*N(k, i, j)) + N(k, i, j) - delta_t*N(k, i, j)*k_N(k, i, j, cancer_cells, normal_cells);
+                M(k + 1, i, j) = gamma * (M(k, i + 1, j) + M(k, i-1, j) + M(k, i, j + 1) + M(k, i, j-1) - 4*M(k, i, j)) + M(k, i, j) - delta_t*M(k, i, j)*k_M(k, i, j, cancer_cells, normal_cells);
 			}
 		}
 	}
